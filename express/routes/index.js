4f31684c5d77bd1205d0cd1ead900df9d90380ef
Rafi Estrada <rafael.estrada@elastic.co>
2022-08-02 21:26:32 -0500

    update before merge into express

--- express/routes/index.js
+++ express/routes/index.js
@@ -1,18 +1,42 @@
 var express = require('express');
 var router = express.Router();
 var cors = require('cors');
+const { Client } = require('@elastic/elasticsearch')
 
 router.use(cors());
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/', (req, res) => {
-  const data = [
-    {"id":"1", "name":"rafi", "city":"bahia"}
-  ]
-  res.send(data)
+
+const client = new Client({
+  node: 'https://localhost:9200',
+  auth: {
+    apiKey: 'UEZfejZZRUI0UTJGN1lpQ2Q2WFY6NmFSZmtmYnNSOWktZnJtRm9BUGlnQQ'
+  },
+  caFingerprint: '8D:85:E7:38:DB:67:CE:43:80:E1:07:F0:51:CB:17:BD:58:D1:90:98:3D:55:DE:61:BD:8E:DC:DF:26:C4:E2:E0',
+  tls: {
+    // might be required if it's a self-signed certificate
+    rejectUnauthorized: false
+  }
 })
+
+/* async function indexDoc () {
+  await client.index({
+    index: 'famm-users',
+    document: req.body
+  })
+  console.log(document)
+} */
+
+router.post('/', function(req, res) {
+  client.index({
+    index: 'famm-users',
+    document: req.body
+  })
+  res.send([req.body])
+});
 
 module.exports = router;
--- user-form/src/App.js
+++ user-form/src/App.js
@@ -1,12 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 import { useState } from "react";
- 
+
 
 function App() {
   const [tipoUsuario, setTipoUsuario] = useState("");
   const [message, setMessage] = useState("");
   const [data, setData] = useState("");
+  const [nombres, setNombres] = useState("");
+  const [apellidos, setApellidos] = useState("");
+  const [cedula, setCedula] = useState("");
+  const [genero, setGenero] = useState("");
+  const [fechaDeNacimiento, setFechaDeNacimiento] = useState("");
+  const [establEscolar, setEstablEscolar] = useState("");
+  const [gradoEscolar, setGradoEscolar] = useState("");
+  const [nombreDelDocente, setNombreDelDocente] = useState("");
+  const [nombrePadre, setNombrePadre] = useState("");
+  const [nombreMadre, setNombreMadre] = useState("");
+  const [nombreTutor, setNombreTutor] = useState("");
+  const [nombreHermanos, setNombreHermanos] = useState("");
+  const [telefonoPrincipal, setTelefonoPrincipal] = useState("");
+  const [telefonoSecundario, setTelefonoSecundario] = useState("");
+  const [email, setEmail] = useState("");
+  const [observaciones, setObservaciones] = useState("");
+  const [lugarDeTrabajo, setlugarDeTrabajo] = useState("");
+  const [entidadQueRepresenta, setEntidadQueRepresenta] = useState("");
+  const [domicilio, setDomicilio] = useState("");
+  const [datosAdjuntos, setDatosAdjuntos] = useState("");
 
   let handleSubmit = async (e) => {
     e.preventDefault();
@@ -15,10 +35,30 @@ function App() {
        method: "POST",
        headers: {
         'Content-Type': 'application/json'
-//	'Authorization': 'Basic ZWxhc3RpYzpCdkR4Ykw5c0s5cDdLOUU1MXUtaA=='       
+//  'Authorization': 'Basic ZWxhc3RpYzpCdkR4Ykw5c0s5cDdLOUU1MXUtaA=='       
       },
        body: JSON.stringify({
-        tipoUsuario: tipoUsuario
+        tipoUsuario: tipoUsuario,
+        nombres: nombres,
+        apellidos: apellidos,
+        cedula: cedula,
+        genero: genero,
+        fechaDeNacimiento: fechaDeNacimiento,
+        establEscolar: establEscolar,
+        gradoEscolar: gradoEscolar,
+        nombreDelDocente: nombreDelDocente,
+        nombrePadre: nombrePadre,
+        nombreMadre: nombreMadre,
+        nombreTutor: nombreTutor,
+        nombreHermanos: nombreHermanos,
+        telefonoPrincipal: telefonoPrincipal,
+        telefonoSecundario: telefonoSecundario,
+        email: email,
+        observaciones: observaciones,
+        lugarDeTrabajo: lugarDeTrabajo,
+        entidadQueRepresenta: entidadQueRepresenta,
+        domicilio: domicilio,
+        datosAdjuntos: datosAdjuntos 
        })
      });
      console.log(res)
@@ -26,6 +66,26 @@ function App() {
      console.log(resJson)
      if (res.status === 200) {
         setTipoUsuario("");
+        setNombres("");
+        setApellidos("");
+        setCedula("");
+        setGenero("");
+        setFechaDeNacimiento("");
+        setEstablEscolar("");
+        setGradoEscolar("");
+        setNombreDelDocente("");
+        setNombrePadre("");
+        setNombreMadre("");
+        setNombreTutor("");
+        setNombreHermanos("");
+        setTelefonoPrincipal("");
+        setTelefonoSecundario("");
+        setEmail("");
+        setObservaciones("");
+        setlugarDeTrabajo("");
+        setEntidadQueRepresenta("");
+        setDomicilio("");
+        setDatosAdjuntos("");
         setMessage('User Created');
         setData(resJson);
       } else {
@@ -35,18 +95,26 @@ function App() {
       console.log(error);
     }
   };
- 
 function testfunc() {
     return (
-      <>
-      
+      <table>
+        
         { data ? 
-          data.map(({ name, city }) => (
-            <p key={name}>My name is {name} i am from {city}.</p>
-            )
-          ) : null
+          <tr key={"header"}>
+          {Object.keys(data[0]).map((key) => (
+            <th>{key}</th>
+          ))}
+          </tr> : null }
+          { data ?
+          data.map((item) => (
+            <tr key={item.cedula}> 
+              {Object.values(item).map((val) => (
+                <td>{val}</td>
+              ))}
+            </tr>
+            )) : null
         }
-      </>
+      </table>
     );
   };
 return (
@@ -55,10 +123,130 @@ return (
         <input
             type="text"
             value={tipoUsuario}
-            placeholder="Tipo De Usuario"
+            placeholder="Tipo de Usuario"
             onChange={(e) => setTipoUsuario(e.target.value)}
         />
-    	  <button type="submit">Create</button>
+        <input
+            type="text"
+            value={nombres}
+            placeholder="Nombres"
+            onChange={(e) => setNombres(e.target.value)}
+        />
+        <input
+            type="text"
+            value={apellidos}
+            placeholder="Apellidos"
+            onChange={(e) => setApellidos(e.target.value)}
+        />
+        <input
+            type="text"
+            value={cedula}
+            placeholder="Número de Cédula"
+            onChange={(e) => setCedula(e.target.value)}
+        />
+        <input
+            type="text"
+            value={genero}
+            placeholder="Género"
+            onChange={(e) => setGenero(e.target.value)}
+        />
+        <input
+            type="text"
+            value={fechaDeNacimiento}
+            placeholder="Fecha de Nacimiento"
+            onChange={(e) => setFechaDeNacimiento(e.target.value)}
+        />
+        <input
+            type="text"
+            value={establEscolar}
+            placeholder="Establecimiento Escolar"
+            onChange={(e) => setEstablEscolar(e.target.value)}
+        />
+        <input
+            type="text"
+            value={gradoEscolar}
+            placeholder="Grado Escolar"
+            onChange={(e) => setGradoEscolar(e.target.value)}
+        />
+        <input
+            type="text"
+            value={nombreDelDocente}
+            placeholder="Nombre del Docente"
+            onChange={(e) => setNombreDelDocente(e.target.value)}
+        />        
+        <input
+            type="text"
+            value={nombrePadre}
+            placeholder="Nombre Padre"
+            onChange={(e) => setNombrePadre(e.target.value)}
+        />        
+        <input
+            type="text"
+            value={nombreMadre}
+            placeholder="Nombre Madre"
+            onChange={(e) => setNombreMadre(e.target.value)}
+        />        
+        <input
+            type="text"
+            value={nombreTutor}
+            placeholder="Nombre Tutor"
+            onChange={(e) => setNombreTutor(e.target.value)}
+        />        
+        <input
+            type="text"
+            value={nombreHermanos}
+            placeholder="Nombre Hermanos"
+            onChange={(e) => setNombreHermanos(e.target.value)}
+        />        
+        <input
+            type="text"
+            value={telefonoPrincipal}
+            placeholder="Telefono Principal"
+            onChange={(e) => setTelefonoPrincipal(e.target.value)}
+        />        
+        <input
+            type="text"
+            value={telefonoSecundario}
+            placeholder="Telefono Secundario"
+            onChange={(e) => setTelefonoSecundario(e.target.value)}
+        />        
+        <input
+            type="text"
+            value={email}
+            placeholder="Email"
+            onChange={(e) => setEmail(e.target.value)}
+        />        
+        <input
+            type="text"
+            value={observaciones}
+            placeholder="Observaciones"
+            onChange={(e) => setObservaciones(e.target.value)}
+        />
+        <input
+            type="text"
+            value={lugarDeTrabajo}
+            placeholder="Lugar de Trabajo"
+            onChange={(e) => setlugarDeTrabajo(e.target.value)}
+        />
+        <input
+            type="text"
+            value={entidadQueRepresenta}
+            placeholder="Entidad que Representa"
+            onChange={(e) => setEntidadQueRepresenta(e.target.value)}
+        />
+        <input
+            type="text"
+            value={domicilio}
+            placeholder="Domicilio"
+            onChange={(e) => setDomicilio(e.target.value)}
+        />
+        <input
+            type="text"
+            value={datosAdjuntos}
+            placeholder="Datos Adjuntos"
+            onChange={(e) => setDatosAdjuntos(e.target.value)}
+        />
+          <button type="submit">Crear</button>
     
         <div> {testfunc()}</div>
 
